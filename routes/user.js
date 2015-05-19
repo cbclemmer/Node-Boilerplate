8f132e2e06027400c510f11541e4cfa721b001f7
Orbweaver <coltonclemmerdev@gmail.com>
2015-05-19 17:26:52 -0400

    login working

--- routes/user.js
+++ routes/user.js
@@ -31,20 +31,25 @@ module.exports = function(app, controllers){
         })
     });
     app.post('/user/login', function(req, res){
-        redis.get("user", function(err, user){
-            if(!user){
-                controllers.user.login(req.query, {
-                    success: function(user){
-                        redis.set('user', user._id);
-                        res.json(user);
-                    }, error: function(error){
-                        res.json({err: error});
-                    }
-                });
-            }else{
-                return res.json({err: "You are logged in"});
-            }
-        });
+        var auth = req.get("auth");
+        if(!auth)
+            return res.json({err: "Invalid format"});
+        else if(auth=="login"){
+            controllers.user.login(req.query, {
+                success: function(user){
+                    redis.get("next_AUTH", function(err, id){
+                        if(!id) id = 0;
+                        redis.set("next_AUTH", id++);
+                        redis.set(id, user._id);
+                        res.json({user: user, auth: id});
+                    });
+                }, error: function(error){
+                    res.json({err: error});
+                }
+            });
+        }else{
+            res.json({err: "Already logged in"});
+        }
     });
     app.post('/user/logout', function(req, res){
         redis.get("user", function(err, user){
